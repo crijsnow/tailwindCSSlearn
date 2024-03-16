8ddddea631bffbe190c4592e2da747d43a962074
JRsama <1208232330@qq.com>
2024-03-16 15:53:54 +0800

    learn1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from 'vue'
+const a=ref('new Page!')
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">{{ a }}</div>
+  <span class="border-sky-200 border-[1px] w-40 h-20 text-center block">测试[]</span>
+</template>
+
+<style scoped>
+
+</style>
